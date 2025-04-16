6396722b201faeace8b386eb221f0c28aae9f51e
fuliqiang <1348994179@qq.com>
2025-04-16 23:54:05 +0800

    feat: add logo in header

--- src/layout/header/index.vue
+++ src/layout/header/index.vue
@@ -9,19 +9,39 @@ const routes = computed(() => router.getRoutes().filter(v => v.meta.menu))
 
 <template>
   <header>
-    <div v-for="(route, index) in routes" :key="index" @click="() => $router.push({ path: (route.redirect || route.path) as string })">
-      <PicButton> {{ route.meta.name }}</PicButton>
-    </div>
+    <!-- <div class="blog-logo">
+      <img src="../../assets/logo.png" alt="">
+    </div> -->
+    <nav>
+      <div v-for="(route, index) in routes" :key="index" @click="() => $router.push({ path: (route.redirect || route.path) as string })">
+        <PicButton> {{ route.meta.name }}</PicButton>
+      </div>
+    </nav>
   </header>
 </template>
 
 <style lang="scss"  scoped>
 header{
-  height: 46px;
+  height: 60px;
+  padding-inline: 24px;
   display: flex;
   justify-content: center;
-  flex-wrap: wrap;
-  gap: 8px;
-  line-height: 46px;
+  background-color: #f7f6f8;
+  nav{
+    flex: 1;
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    gap: 8px;
+    line-height: 60px;
+  }
+  .blog-logo{
+    height: 100%;
+    img{
+      width: 100%;
+      height: 100%;
+      object-fit: contain;
+    }
+  }
 }
 </style>
--- src/view/home.vue
+++ src/view/home.vue
@@ -2,8 +2,23 @@
 </script>
 
 <template>
-  <div class="flex" />
+  <div class="home">
+    <h1>Welcome Fdefined's world !</h1>
+  </div>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+.home{
+  padding: 24px;
+  height: 100%;
+  background-color: #f6f7f8;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  h1{
+    text-align: center;
+    letter-spacing: 20px;
+    font-size: 32px;
+  }
+}
 </style>
