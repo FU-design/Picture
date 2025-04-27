476294795074f525a00238ecc1eea084dc202af0
fuliqiang <1348994179@qq.com>
2025-04-27 17:21:47 +0800

    fix: fix base value of vite config

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ command, mode }) => {
   console.log('command :>> ', command)
   console.log('mode :>> ', mode)
   return {
-    base: '/pic/', // (当使用 github pages部署的时候，需要配置该 base)
+    base: '/Picture/', // (当使用 github pages部署的时候，需要配置该 base: '/<仓库>/')
     plugins: [vue()],
     resolve: {
       alias: {
