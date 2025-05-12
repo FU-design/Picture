df4544fe92301201f5b24975d295fccd6e07cd69
fuliqiang <1348994179@qq.com>
2025-05-12 15:34:35 +0800

    feat: 实现防抖函数

---
+++ src/notes/handwrite/debounce.ts
@@ -0,0 +1,75 @@
+// 简单版本
+export function debounce(func: any, delay: number) {
+  let timer: NodeJS.Timeout
+  return (...args: any) => {
+    if (timer)
+      clearTimeout(timer)
+    timer = setTimeout(() => {
+      func(args)
+    }, delay)
+  }
+}
+
+// 可选择是否在第一次触发时，立即执行操作
+export function debounceImmediate(func: any, delay: number, immediate: boolean = false) {
+  let timer: ReturnType<typeof setTimeout> | null = null
+  let result: any
+  let _this: any = null
+  let _args: any[] | null = null
+
+  const debounced = function (this: any, ...args: any) {
+    _this = this!
+    _args = args
+
+    const callNow = immediate && timer == null
+    clearTimeout(timer!)
+    timer = setTimeout(() => {
+      timer = null
+      if (!immediate && _args) {
+        result = func.apply(_this, _args)
+      }
+    }, delay)
+
+    if (callNow) {
+      result = func.apply(_this, _args)
+    }
+
+    return result
+  }
+
+  return debounced
+}
+
+// 可在防抖过程中取消后续操作
+export function debounceCancel(func: any, delay: number) {
+  let timer: ReturnType<typeof setTimeout> | null = null
+  let result: any
+  let _this: any = null
+  let _args: any[] | null = null
+
+  const debounced = function (this: any, ...args: any) {
+    _this = this!
+    _args = args
+
+    clearTimeout(timer!)
+    timer = setTimeout(() => {
+      timer = null
+      if (_args) {
+        result = func.apply(_this, _args)
+      }
+    }, delay)
+    return result
+  }
+
+  debounced.cancel = () => {
+    if (timer !== null) {
+      clearTimeout(timer)
+      timer = null
+      _this = null
+      _args = null
+      return result
+    }
+  }
+
+  return debounced
+}
