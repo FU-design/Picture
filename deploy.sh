16847c152bdd61d623f1f5ae88ba744434a1a561
fuliqiang <1348994179@qq.com>
2025-08-11 17:34:01 +0800

    chore: fix script

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# 提示用户输入信息
+# 提示输入信息
 read -p "Enter server IP: " SERVER_IP
 read -p "Enter project directory on server (e.g. /opt/Picture): " PROJECT_DIR
 read -p "Enter Git branch to pull (default: main): " GIT_BRANCH
 
-# 如果用户没输入分支，默认用 main
+# 默认用 main
 GIT_BRANCH=${GIT_BRANCH:-main}
 
 echo "Deploying to $SERVER_IP, project dir: $PROJECT_DIR, branch: $GIT_BRANCH"
 
-# 这里用 root 用户，如果你需要换用户，改下面这一行即可
+# 云服务器 root 用户
 SSH_USER="root"
 
 # ssh 执行远程命令
